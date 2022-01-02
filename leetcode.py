45de023a872d996c3c805a9b831482ec083b2bbb
AdityaArakere <aditya.arakere@gmail.com>
2022-01-02 15:27:44 +0530

    first commit

---
+++ leetcode.py
@@ -0,0 +1,12 @@
+class Solution:
+    
+    def twoSum(self, nums: List[int], target: int) -> List[int]:    
+        #temp1=[]
+        l=len(nums)
+        for i in range(0,l):
+            for j in range(1,l):
+                if nums[i] + nums[j] ==target:
+                    if i!=j:
+                        return(i,j)
+                        #temp1=[i,j]
+        #return temp1
